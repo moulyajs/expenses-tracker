d6e78e2a729fd2fd314e128fbc66a7ad661cf81c
prajwalmediaant <prajwal@themediaant.com>
2023-09-04 20:46:47 +0530

    used props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
+
+import ExpenseDate from './ExpenseDate';
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
- const expenseDate = new Date(2023, 5 , 18);
- const expenseTitle = `Car insurance`;
- const expenseAmount = 100;
+function ExpenseItem(props) {
+   
 
   return (
     <div className ="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
+    
       <div className ="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className  ="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className  ="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
